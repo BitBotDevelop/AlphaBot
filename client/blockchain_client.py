4bb04b447d6ca82ff81c4ac45fc04c6301800da6
zhgsnake <zhgsnake@163.com>
2023-11-20 17:20:22 +0800

    Update blockchain_client.py

--- client/blockchain_client.py
+++ client/blockchain_client.py
@@ -1,22 +1,50 @@
 import client.http_client
+import time
 
 
-def get_latest_block_number():
+def get_latest_block():
     """
         查询最新的区块高度
     """
     url = "https://blockchain.info/latestblock"
-    print("query latest block number, url:{}".format(url))
+    print("query latest block, url:{}".format(url))
 
     try:
         response = client.http_client.get(url)
         if response is not None:
-            return response['height']
+            return response
         else:
             return 0
     except Exception as e:
-        print("call blockchain api to query latest block number error, message:", e)
+        print("call blockchain api to query latest block error, message:", e)
         return 0
+
+
+def get_one_day_blocks():
+    """
+        获取一天内的区块详情
+        :return
+        [
+            {
+                "hash":"000000000000000000037d949006b5aa937a6da3979c843b24edcb6e3982e1bc",
+                "height":817615,
+                "time":1700464150,
+                "block_index":817615
+            }
+        ]
+    """
+    import time
+    current_time = int(time.time() * 1000)
+    try:
+        url = "https://blockchain.info/blocks/{}?format=json".format(current_time)
+        response = client.http_client.get(url)
+        if response is not None:
+            return response
+        else:
+            return None
+    except Exception as e:
+        print("call blockchain api to get blocks in last day,message:", e)
+        return None
 
 
 def get_gas_detail():

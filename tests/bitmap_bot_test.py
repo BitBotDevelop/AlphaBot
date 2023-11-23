2428648a5cbd4f0c5027f5b8a7e7eea193b36ba5
zhgsnake <zhgsnake@163.com>
2023-11-23 23:39:46 +0800

    update

--- tests/bitmap_bot_test.py
+++ tests/bitmap_bot_test.py
@@ -1,7 +1,7 @@
 from service.bitmap_bot import *
 from client.wallet_client import *
 from strategy.bid_strategy import *
-from  common.common import *
+from common.common import *
 import os
 
 api_keys = ["ecce7b6848238930e191cddc0a8bc9fcb6b442a0fe0248dbab175a85794b548d"]

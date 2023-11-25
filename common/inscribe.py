d19c3dbf24a6af3f9b5f74673e49c18c8e11bc27
Robot.l <lbh9311@sina.com>
2023-11-25 17:20:04 +0800

    feat: add inscribe cookbook

--- common/inscribe.py
+++ common/inscribe.py
@@ -60,6 +60,9 @@ def get_bitmap_inscription_address(private: str, block_nummber: int):
   return from_address.to_string()
 
 def inscribe_after_tx(txid: str, vout: int, amount: int, private: str, block_nummber: int, receive_address: str):
+  """
+  铭刻交易
+  """
   # taproot script is a simple P2PK with the following keys
   privkey_tr_script = PrivateKey.from_bytes(bytes.fromhex(private))
   pubkey_tr_script = privkey_tr_script.get_public_key()
@@ -67,9 +70,7 @@ def inscribe_after_tx(txid: str, vout: int, amount: int, private: str, block_num
 
   # taproot script path address
   from_address = pubkey_tr_script.get_taproot_address([[tr_script_p2pk]])
-
   print("From Taproot script address", from_address.to_string())
-
 
   # create transaction input from tx id of UTXO
   tx_in = TxInput(txid, vout)
@@ -80,14 +81,9 @@ def inscribe_after_tx(txid: str, vout: int, amount: int, private: str, block_num
   utxos_scriptPubkeys = [scriptPubkey]
   toAddress = P2trAddress(receive_address)
 
-
   tx_out = TxOutput(546, toAddress.to_script_pub_key())
   tx = Transaction([tx_in], [tx_out], has_segwit=True)
 
-  print("\nRaw transaction:\n" + tx.serialize())
-
-  print("\ntxid: " + tx.get_txid())
-  print("\ntxwid: " + tx.get_wtxid())
 
   # sign taproot input
   # to create the digest message to sign in taproot we need to
@@ -111,13 +107,121 @@ def inscribe_after_tx(txid: str, vout: int, amount: int, private: str, block_num
       TxWitnessInput([sig, tr_script_p2pk.to_hex(), control_block.to_hex()])
   )
   # print raw signed transaction ready to be broadcasted
-  print("\nRaw signed transaction:\n" + tx.serialize())
+  return [tx.serialize(), tx.get_vsize(), tx.get_txid()]
 
-  print("\nTxId:", tx.get_txid())
-  print("\nTxwId:", tx.get_wtxid())
+def inscribe_and_send_after_tx(
+    txid: str,
+    vout: int,
+    amount: int,
+    fee: int,
+    private: str,
+    block_nummber: int,
+    receive_address: str,
+    send_address: str,
+  ):
+  # taproot script is a simple P2PK with the following keys
+  privkey_tr_script = PrivateKey.from_bytes(bytes.fromhex(private))
+  pubkey_tr_script = privkey_tr_script.get_public_key()
+  tr_script_p2pk = get_bitmap_script_pub_key(private, block_nummber)
 
-  print("\nSize:", tx.get_size())
-  print("\nvSize:", tx.get_vsize())
-  return [tx.serialize(), tx.get_vsize()]
+  # taproot script path address
+  from_address = pubkey_tr_script.get_taproot_address([[tr_script_p2pk]])
+  print("From Taproot script address", from_address.to_string())
+
+  # create transaction input from tx id of UTXO
+  tx_in = TxInput(txid, vout)
+
+  amounts = [amount]
+
+  scriptPubkey = from_address.to_script_pub_key()
+  utxos_scriptPubkeys = [scriptPubkey]
+  toAddress = P2trAddress(receive_address)
+  refundAddress = P2trAddress(send_address)
+
+  base = 546
+  tx_out_list = []
+  if amount - base > 0:
+    tx_out_inscription = TxOutput(base, toAddress.to_script_pub_key())
+    tx_out_list.append(tx_out_inscription)
+  else:
+    tx_out_inscription = TxOutput(amount, toAddress.to_script_pub_key())
+    tx_out_list.append(tx_out_inscription)
+  if amount - base - fee > 0:
+    tx_out_refund = TxOutput(amount - fee - base, refundAddress.to_script_pub_key())
+    tx_out_list.append(tx_out_refund)
+
+  print(tx_out_list)
+  
+  tx = Transaction([tx_in], tx_out_list, has_segwit=True)
+  # sign taproot input
+  # to create the digest message to sign in taproot we need to
+  # pass all the utxos' scriptPubKeys, their amounts and taproot script
+  # we sign with the private key corresponding to the script - no keys
+  # tweaking required
+  sig = privkey_tr_script.sign_taproot_input(
+      tx,
+      0,
+      utxos_scriptPubkeys,
+      amounts,
+      script_path=True,
+      tapleaf_script=tr_script_p2pk,
+      tweak=False,
+  )
+
+  # we spend a single script - no merkle path is required
+  control_block = ControlBlock(pubkey_tr_script)
+
+  tx.witnesses.append(
+      TxWitnessInput([sig, tr_script_p2pk.to_hex(), control_block.to_hex()])
+  )
+  # print raw signed transaction ready to be broadcasted
+  return [tx.serialize(), tx.get_vsize(), tx.get_txid()]
+
+def refund_from_inscription_tx(
+    txid: str,
+    vout: int,
+    amount: int,
+    fee: int,
+    private: str,
+    block_nummber: int,
+    receive_address: str,
+):
+  privkey_tr_script = PrivateKey.from_bytes(bytes.fromhex(private))
+  pubkey_tr_script = privkey_tr_script.get_public_key()
+  tr_script_p2pk = get_bitmap_script_pub_key(private, block_nummber)
+
+  # taproot script path address
+  from_address = pubkey_tr_script.get_taproot_address([[tr_script_p2pk]])
+
+  # create transaction input from tx id of UTXO
+  tx_in = TxInput(txid, vout)
+
+  amounts = [amount]
+
+  scriptPubkey = from_address.to_script_pub_key()
+  utxos_scriptPubkeys = [scriptPubkey]
+  toAddress = P2trAddress(receive_address)
+
+  tx_out_list = []
+  tx_out_inscription = TxOutput(amount - fee, toAddress.to_script_pub_key())
+  tx_out_list.append(tx_out_inscription)
+
+  print(tx_out_list)
+  
+  tx = Transaction([tx_in], tx_out_list, has_segwit=True)
+
+
+  sig = privkey_tr_script.sign_taproot_input(
+        tx,
+        0,
+        utxos_scriptPubkeys,
+        amounts,
+        script_path=False,
+        tapleaf_scripts=[tr_script_p2pk],
+    )
+
+  tx.witnesses.append(TxWitnessInput([sig]))
+  # print raw signed transaction ready to be broadcasted
+  return [tx.serialize(), tx.get_vsize(), tx.get_txid()]
 
 
